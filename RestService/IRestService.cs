23612ab1b4cca9d2e81b1f931092f30432e03528
KR-Eddie <luiseddie.kr@gmail.com>
2016-05-07 12:41:08 +0100

    Initial commit: From example "Restfull services demo"

---
+++ RestService/IRestService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RestService
+{
+    [ServiceContract]
+    public interface IRestService {
+
+        [WebGet(UriTemplate = "/users", ResponseFormat = WebMessageFormat.Json)]
+        [Description("Gets all users stored so far.")]
+        [OperationContract]
+        Users GetUsers();
+
+    }
+
+    [CollectionDataContract(Name="users", Namespace="")]
+    public class Users : List<User> {
+    }
+
+    [DataContract(Name="user", Namespace="")]
+    public class User {
+    }
+}
---
+++ RestService/RestService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace RestService
+{
+    public class RestService : IRestService
+    {
+        static Users users = new Users();
+
+        public Users GetUsers() {
+            return users;            
+        }
+    }
+}
