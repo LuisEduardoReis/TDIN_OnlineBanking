9dfbddd348c098982c965f62a3cfc656c1941ad5
Susana <si.ventura1@gmail.com>
2016-05-24 16:47:46 +0100

    added counter project counter project layout

---
+++ Counter/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbCompany = new System.Windows.Forms.ComboBox();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstOrders = new System.Windows.Forms.ListView();
+            this.btnChangeUser = new System.Windows.Forms.Button();
+            this.lblUserIdError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User id";
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(89, 10);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(201, 20);
+            this.txtUserId.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "New order";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Company";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Type";
+            // 
+            // cmbCompany
+            // 
+            this.cmbCompany.FormattingEnabled = true;
+            this.cmbCompany.Location = new System.Drawing.Point(89, 143);
+            this.cmbCompany.Name = "cmbCompany";
+            this.cmbCompany.Size = new System.Drawing.Size(201, 21);
+            this.cmbCompany.TabIndex = 6;
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(89, 177);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(201, 21);
+            this.cmbType.TabIndex = 7;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(89, 113);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(201, 20);
+            this.txtQuantity.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "User\'s orders";
+            // 
+            // lstOrders
+            // 
+            this.lstOrders.Location = new System.Drawing.Point(340, 67);
+            this.lstOrders.Name = "lstOrders";
+            this.lstOrders.Size = new System.Drawing.Size(242, 236);
+            this.lstOrders.TabIndex = 10;
+            this.lstOrders.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnChangeUser
+            // 
+            this.btnChangeUser.Location = new System.Drawing.Point(313, 10);
+            this.btnChangeUser.Name = "btnChangeUser";
+            this.btnChangeUser.Size = new System.Drawing.Size(75, 20);
+            this.btnChangeUser.TabIndex = 11;
+            this.btnChangeUser.Text = "Go!";
+            this.btnChangeUser.UseVisualStyleBackColor = true;
+            this.btnChangeUser.Click += new System.EventHandler(this.btnChangeUser_Click);
+            // 
+            // lblUserIdError
+            // 
+            this.lblUserIdError.AutoSize = true;
+            this.lblUserIdError.Location = new System.Drawing.Point(409, 13);
+            this.lblUserIdError.Name = "lblUserIdError";
+            this.lblUserIdError.Size = new System.Drawing.Size(0, 13);
+            this.lblUserIdError.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 341);
+            this.Controls.Add(this.lblUserIdError);
+            this.Controls.Add(this.btnChangeUser);
+            this.Controls.Add(this.lstOrders);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.cmbCompany);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtUserId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbCompany;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView lstOrders;
+        private System.Windows.Forms.Button btnChangeUser;
+        private System.Windows.Forms.Label lblUserIdError;
+    }
+}
+
---
+++ Counter/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Models;
+
+namespace Counter
+{
+    public partial class Form1 : Form
+    {
+        int current_user;
+        string hostUrl;
+
+
+        public Form1(string hostUrl)
+        {
+            InitializeComponent();
+            this.hostUrl = hostUrl;
+            current_user = -1;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChangeUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                current_user = Int32.Parse(txtUserId.Text);
+            }
+            catch (Exception)
+            {
+                lblUserIdError.Text = "Id must be an integer";
+                return;
+            }
+
+            lblUserIdError.Text = "";
+
+            String clients = Util.GetRequest(hostUrl + "/clients/"+current_user);
+            if (clients == null) lblUserIdError.Text = "Client not found";
+            else {
+                lblUserIdError.Text = "";
+
+                txtQuantity.Text = "";
+                cmbCompany.SelectedIndex = 0;
+                cmbType.SelectedIndex = 0;
+
+
+
+
+            }
+
+
+
+        }
+    }
+}
